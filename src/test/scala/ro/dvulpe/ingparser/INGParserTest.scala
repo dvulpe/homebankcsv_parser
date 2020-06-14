6eebeabda48d0ab63fa192d49e1cbfffad5a8d11
Dan Vulpe <dvulpe@gmail.com>
2020-06-14 13:06:17 +0100

    refactor assertions

--- src/test/scala/ro/dvulpe/ingparser/INGParserTest.scala
+++ src/test/scala/ro/dvulpe/ingparser/INGParserTest.scala
@@ -12,7 +12,10 @@ class INGParserTest extends AnyFunSuite {
   }
 
   test("transaction details lines should be parsed") {
-    val input = ",Beneficiar: RCS AND RDS SA,,\n,Banca: INGB CENTRALA,,\n,Referinta: 70979872,,"
+    val input =
+      """,Beneficiar: RCS AND RDS SA,,
+        |,Banca: INGB CENTRALA,,
+        |,Referinta: 70979872,,""".stripMargin
     val result = INGParser.parseTest(input, INGParser.transDetails)
     assert(result === List("Beneficiar: RCS AND RDS SA", "Banca: INGB CENTRALA", "Referinta: 70979872"))
   }
@@ -24,33 +27,91 @@ class INGParserTest extends AnyFunSuite {
   }
 
   test("transaction with details should be parsed") {
-    val input = "30 septembrie 2013,Plata debit direct,\"120,19\",\n,Beneficiar: RCS AND RDS SA,,\n,Banca: INGB CENTRALA,,\n,Referinta: 70979872,,"
+    val input =
+      """30 septembrie 2013,Plata debit direct,"120,19",
+        |,Beneficiar: RCS AND RDS SA,,
+        |,Banca: INGB CENTRALA,,
+        |,Referinta: 70979872,,""".stripMargin
     val result = INGParser.parseTest(input, INGParser.ingRecord)
-    assert(result != null)
+    assert(result === IngRecord(
+      summary = Summary(
+        date = LocalDate.of(2013, 9, 30),
+        details = "Plata debit direct", debit = Some(BigDecimal("120.19")), credit = None),
+      details = "Beneficiar: RCS AND RDS SA" :: "Banca: INGB CENTRALA" :: "Referinta: 70979872" :: Nil)
+    )
   }
 
   test("transaction without details should be parsed") {
-    val input = "30 septembrie 2013,Plata debit direct,\"120,19\",\n"
+    val input =
+      """30 septembrie 2013,Plata debit direct,"120,19",
+        |""".stripMargin
     val result = INGParser.parseTest(input, INGParser.ingRecord)
-    assert(result != null)
+    assert(result === IngRecord(
+      summary = Summary(
+        date = LocalDate.of(2013, 9, 30),
+        details = "Plata debit direct",
+        debit = Some(BigDecimal("120.19")),
+        credit = None
+      ),
+      details = Seq.empty
+    ))
   }
 
   test("two consecutive transactions should be parsed") {
-    val input = "30 septembrie 2013,Plata debit direct,\"120,19\",\n30 septembrie 2013,Plata debit direct,\"120,19\",\n"
+    val input =
+      """30 septembrie 2013,Plata debit direct,"120,19",
+        |30 septembrie 2013,Plata debit direct,"120,19",
+        |""".stripMargin
     val result = INGParser.parseRecords(input)
-    assert(result.size === 2)
-  }
-
-  test("header should be matched") {
-    val input = "Data,Detalii tranzactie,Debit,Credit\n"
-    val result = INGParser.parseTest(input, INGParser.header)
-    assert(result != null)
+    assert(result === List(
+      IngRecord(
+        summary = Summary(
+          date = LocalDate.of(2013, 9, 30),
+          details = "Plata debit direct",
+          debit = Some(BigDecimal("120.19")),
+          credit = None
+        ),
+        details = Seq.empty
+      ),
+      IngRecord(
+        summary = Summary(
+          date = LocalDate.of(2013, 9, 30),
+          details = "Plata debit direct",
+          debit = Some(BigDecimal("120.19")),
+          credit = None
+        ),
+        details = Seq.empty
+      )
+    ))
   }
 
   test("transaction fiel with header should be matched") {
-    val input = "Data,Detalii tranzactie,Debit,Credit\n30 septembrie 2013,Plata debit direct,\"120,19\",\n30 septembrie 2013,Plata debit direct,\"120,19\",\n"
+    val input =
+      """Data,Detalii tranzactie,Debit,Credit
+        |30 septembrie 2013,Plata debit direct,"120,19",
+        |30 septembrie 2013,Plata debit direct,"120,19",
+        |""".stripMargin
     val result = INGParser.parseRecords(input)
-    assert(result.size === 2)
+    assert(result === List(
+      IngRecord(
+        summary = Summary(
+          date = LocalDate.of(2013, 9, 30),
+          details = "Plata debit direct",
+          debit = Some(BigDecimal("120.19")),
+          credit = None
+        ),
+        details = Seq.empty
+      ),
+      IngRecord(
+        summary = Summary(
+          date = LocalDate.of(2013, 9, 30),
+          details = "Plata debit direct",
+          debit = Some(BigDecimal("120.19")),
+          credit = None
+        ),
+        details = Seq.empty
+      )
+    ))
   }
 
 }
